628f1d640602c4be00d1f485fab1a65a2bcae85a
mrtmrtmlck <mertmuratmalcok@gmail.com>
2014-11-03 20:05:48 +0200

    Update simple.scala

--- HW-01/simple.scala
+++ HW-01/simple.scala
@@ -1,11 +1,11 @@
 object simple {
   def main(args: Array[String]) {
-    val list = Array[Int](10, 25, 30);
-    list.foreach((number: Int) => {
+    val simpleList = Array[Int](10, 25, 30);
+    simpleList.foreach((number: Int) => {
       if (number % 2 == 0) {
-        println(number*2);
-      } else println(number*3)
+        println(number * 2);
+      } else 
+        println(number * 3)
     })
-
   }
 }
