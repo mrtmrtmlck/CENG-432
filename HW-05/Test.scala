320dbb494a7cf1a5056fb2eeaf831f22326ecbd2
mrtmrtmlck <mertmuratmalcok@gmail.com>
2014-12-08 22:17:34 +0200

    Update Test.scala

--- HW-05/Test.scala
+++ HW-05/Test.scala
@@ -9,7 +9,7 @@ object Test extends App {
   println(RandomStuff.allValid(List(1, 2, 3), (x: Int) => x > 100));
 
   println("\nexecuteWithRetry:");
-  println(RandomStuff.executeWithRetry(12, (13 / 0)));
-  println(RandomStuff.executeWithRetry(7, (22 / 5)));
+  println(RandomStuff.executeWithRetry(3, (5 / 2)));
+  println(RandomStuff.executeWithRetry(3, (5 / 0)));
 
 }
