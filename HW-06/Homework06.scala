cb32268cfa3e7764e7bd9a4852c3f5fb4445829e
mrtmrtmlck <mertmuratmalcok@gmail.com>
2014-12-15 23:23:22 +0200

    Update Homework06.scala

--- HW-06/Homework06.scala
+++ HW-06/Homework06.scala
@@ -1,1 +1,29 @@
- 
+case class User(name: String, email: String, grade: Int)
+trait UserManagerTrait {
+  def add(name: String, email: String, grade: Int): Option[User]
+  def getUser(email: String): Option[User]
+  def getUserListForGrade(grade: Int): List[User]
+  def getCertainGrades(gradeSelector: (Int) => Boolean): List[String]
+}
+object UserManager extends UserManagerTrait {
+  var userSet = Set[User]()
+
+  def add(name: String, email: String, grade: Int): Option[User] = {
+    val user = User(name, email, grade)
+    if (userSet.contains(user))
+      None
+    else {
+      userSet += user;
+      Option(user)
+    }
+  }
+  def getUser(email: String): Option[User] = {
+    userSet.find(u => u.email == email)
+  }
+  def getUserListForGrade(grade: Int): List[User] = {
+    userSet.filter(u => u.grade == grade).toList
+  }
+  def getCertainGrades(gradeSelector: (Int) => Boolean): List[String] = {
+    userSet.filter(u => gradeSelector(u.grade)).map(u => u.name).toList
+  }
+}
